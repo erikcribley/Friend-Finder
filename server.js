2db8b317c5770a6eeb25d378008cadb3f2dd3d2f
Erik Cribley <erikcribley@gmail.com>
2019-08-01 18:12:35 -0400

    server, routes

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+//dependencies
 let express = require("express")
 let path = require("path")
 
+//express
 let app = express()
-let port = 3000
+let PORT = 3000
+
+//parsing
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"))
+})
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"))
+})
+
+//starts server
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT)
+})
