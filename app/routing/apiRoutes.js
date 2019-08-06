2cf7ae81f44e753188511863c8dced82338919bf
Erik Cribley <erikcribley@gmail.com>
2019-08-05 23:38:15 -0400

    jquery for survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,17 @@
 //dependencies
 let express = require("express")
 let path = require("path")
+let friends = require("../data/friends.js")
 
 //set up router
 const apiRoutes = express.Router()
-// apiRoutes
-//     .get('/api/friends', function(req, res) {
-//         res.json()
-//     })
-//     .post()
+apiRoutes
+    .get('/api/friends', function(req, res) {
+        res.json("friends")
+    })
+    .post('/api/friends', function (req, res) {
 
+    })
+    
 //export router
 module.exports = apiRoutes
